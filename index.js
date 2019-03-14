5244d04778009cbcc983416d3ca7b4b297b7c45a
Aliya Lewis <aliyalewis5@gmail.com>
2019-03-14 21:02:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase())
     return "I can\'t hear you!"; {
-  } else if (string.toUpperCase())
+  } else (string.toUpperCase())
     return "YES INDEED!"; {
   } else "I love you Grandma."
     return "I love you too.";
